300a2fcd6dba4cd4bbe4cafdd96add5daf387cd7
afloradda <92307499+afloradda@users.noreply.github.com>
2025-11-05 13:09:43 -0300

    feat/commit inicial: configurando conexão ao banco de dados (application.properties) e limitando arquivos (.gitignore)

---
+++ src/main/java/com/demo/almoxarifado_fifo/AlmoxarifadoFifoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.almoxarifado_fifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmoxarifadoFifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmoxarifadoFifoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/almoxarifado_fifo/AlmoxarifadoFifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.almoxarifado_fifo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxarifadoFifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
